06ed266c13b60dab28a12d619b602f669cc856f1
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2019-01-19 01:49:39 -0200

    Fix font size dimensions

--- src/config/Metrics.js
+++ src/config/Metrics.js
@@ -2,13 +2,9 @@ import { Dimensions } from 'react-native';
 
 export const { width, height } = Dimensions.get('screen');
 
-/*
-tam. fonte 20 -> 2.6
-tam. fonte 19 -> 2.5
-tam. fonte 17 -> 2.3
-tam. fonte 15 -> 2.1
-*/
-
 export const fontSizeResponsive = value => {
-  return Math.sqrt(height * height + width * width) * (value / 100);
+  const tempHeight = (16 / 9) * width;
+  return (
+    Math.sqrt(Math.pow(tempHeight, 2) + Math.pow(width, 2)) * (value / 100)
+  );
 };
