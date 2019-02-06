830ff826cb05b43ecb9afd24ab116f08b888d5d3
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2019-02-06 00:41:35 -0200

    Add test

---
+++ app/components/__tests__/Modal.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View } from 'react-native';
+import { render } from 'react-native-testing-library';
+import { Modal } from '../Modal';
+
+test('should verify if renders modal', () => {
+  const { getByType } = render(
+    <Modal>
+      <View />
+    </Modal>
+  );
+  const modal = getByType('Modal');
+
+  expect(getByType('Modal')).toBe(modal);
+});
