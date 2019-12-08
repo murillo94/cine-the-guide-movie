650951bbe3c44f64cbefc2f08741235ff3979770
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2019-12-08 09:37:11 -0300

    Ordering props

--- app/components/modals/Modal.js
+++ app/components/modals/Modal.js
@@ -2,21 +2,21 @@ import React from 'react';
 import Modal from 'react-native-modal';
 
 const ModalCustom = ({
+  children = null,
   isVisible = false,
-  onModalShow = () => null,
-  onClose = () => null,
   style = {},
-  children = null
+  onModalShow = () => null,
+  onClose = () => null
 }) => (
   <Modal
     isVisible={isVisible}
+    useNativeDriver
+    hideModalContentWhileAnimating
+    backdropOpacity={0.5}
+    style={style}
     onModalShow={onModalShow}
     onBackdropPress={onClose}
     onBackButtonPress={onClose}
-    hideModalContentWhileAnimating
-    useNativeDriver
-    backdropOpacity={0.5}
-    style={style}
   >
     {children}
   </Modal>
