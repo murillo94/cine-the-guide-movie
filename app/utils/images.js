3cde12b0464b4da3937d54915d7a34a3462074c6
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2019-12-10 12:17:33 -0300

    Adding key param

--- app/utils/images.js
+++ app/utils/images.js
@@ -1,5 +1,5 @@
 /* eslint-disable global-require */
 export const notFound = require('../assets/images/not_found.png');
 
-export const getImageApi = (path, width = 'w500') =>
-  path ? { uri: `https://image.tmdb.org/t/p/${width}/${path}` } : notFound;
+export const getImageApi = (path, key = 'uri', width = 'w500') =>
+  path ? { [key]: `https://image.tmdb.org/t/p/${width}/${path}` } : notFound;
