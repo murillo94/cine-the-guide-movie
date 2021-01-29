4963baddbe577b55f93403d5d33793df188fda27
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2021-01-29 02:18:43 -0300

    Fix image modal test

--- app/components/__tests__/ImagesModal.test.js
+++ app/components/__tests__/ImagesModal.test.js
@@ -4,7 +4,7 @@ import { render } from '@testing-library/react-native';
 import ImagesModal from '../modals/ImagesModal';
 
 test('should verify if renders images modal', () => {
-  const { getByTestId } = render(<ImagesModal />);
+  const { container } = render(<ImagesModal />);
 
-  expect(getByTestId('modal')).toBeTruthy();
+  expect(container).toBeTruthy();
 });
